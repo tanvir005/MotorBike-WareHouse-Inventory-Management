1bc1df99991745b38dfbbc83e91cfc4b8e02fe1f
tanvir005 <tanvir3550gmail.com>
2022-05-08 21:54:06 +0600

    Add items post method added

--- src/Items/AddItems/AddItems.js
+++ src/Items/AddItems/AddItems.js
@@ -3,10 +3,21 @@ import { useForm } from "react-hook-form";
 
 const AddItems = () => {
     const { register, handleSubmit } = useForm();
-
+    const onSubmit = data => {
+        fetch('http://localhost:5000/invenrotyitems', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => console.log(result))
+        console.log(data);
+    }
     return (
         <div>
-            <form className='d-flex flex-column' onSubmit={""}>
+            <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
                 <input className='mb-2' placeholder="Name" {...register("name")} />
                 <textarea className='mb-2' placeholder="Description" {...register("description")} />
                 <input className='mb-2' placeholder="Price" type="number" {...register("price")} />
