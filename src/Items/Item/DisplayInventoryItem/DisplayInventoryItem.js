d04da62b29cd579d6e1160d8f3332e2221f80c1b
tanvir005 <tanvir3550gmail.com>
2022-05-08 23:23:39 +0600

    Single Inventory item

--- src/Items/Item/DisplayInventoryItem/DisplayInventoryItem.js
+++ src/Items/Item/DisplayInventoryItem/DisplayInventoryItem.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 
-const DisplayInventoryItem = () => {
+const DisplayInventoryItem = ({ item }) => {
+
+    const { _id, name, img, description, price } = item;
+
+    const navigate = useNavigate();
+
+    const navigateToCheckOut = id => {
+        navigate(`/inventoryitems/${id}`)
+    }
+
     return (
         <div className="flex gap-10 m-10 p-5 border-2 shadow-slate-50 rounded-2xl">
 
             <div>
                 <img className="w-80 h-64 mx-auto hover:rotate-2" src={img} alt="" />
                 <h3 className="text-2xl font-bold text-left ">{name}</h3>
-                <p className="text-slate-500 text-justify">{details}</p>
+                <p className="text-slate-500 text-justify">{description}</p>
                 <p className="text-black text-justify font-bold text-lg my-5">Price:  ${price}</p>
 
-                <button onClick={() => navigateToCheckOut(name)} className=" bg-slate-800 hover:bg-slate-700 text-white font-bold py-2 w-full rounded focus:outline-none focus:shadow-outline" type="button">
-                    Book Now
+                <button onClick={() => navigateToCheckOut(_id)} className=" bg-slate-800 hover:bg-slate-700 text-white font-bold py-2 w-full rounded focus:outline-none focus:shadow-outline" type="button">
+                    Manage Item
                 </button>
 
             </div>
